8ba9fb8622d426ad8e285cf7f32a847d569b4df2
SasankG <sasankgraju@gmail.com>
2020-03-31 13:27:18 -0400

    Updates

---
+++ precache-manifest.77d23b3b69234cd648c2e47d27fae7c7.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "82afdd0a0ec70d8b721926d58c054b6c",
+    "url": "/asialicuos-2020/index.html"
+  },
+  {
+    "revision": "9c9808a6d89cfa3e3c4d",
+    "url": "/asialicuos-2020/static/css/main.8f433f89.chunk.css"
+  },
+  {
+    "revision": "145262b94818a019c236",
+    "url": "/asialicuos-2020/static/js/2.b861633e.chunk.js"
+  },
+  {
+    "revision": "94b29ac6d0ec426932e82da99c88b81c",
+    "url": "/asialicuos-2020/static/js/2.b861633e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9c9808a6d89cfa3e3c4d",
+    "url": "/asialicuos-2020/static/js/main.0f186baf.chunk.js"
+  },
+  {
+    "revision": "ff81885459834f76cc92",
+    "url": "/asialicuos-2020/static/js/runtime-main.253b4426.js"
+  },
+  {
+    "revision": "f1389e0b22272f895d5ebdc0689d796d",
+    "url": "/asialicuos-2020/static/media/AsialiciousDecal02.f1389e0b.png"
+  },
+  {
+    "revision": "f2800c2e07d7b845aee3f881bf452553",
+    "url": "/asialicuos-2020/static/media/AsialiciousWebsiteFront.f2800c2e.jpg"
+  },
+  {
+    "revision": "a4e897279915b0b7a6595cb7bd2189c3",
+    "url": "/asialicuos-2020/static/media/logo-grey.a4e89727.png"
+  },
+  {
+    "revision": "06e6dd9dec7fe30a401638e29edc3e28",
+    "url": "/asialicuos-2020/static/media/logo-pink.06e6dd9d.png"
+  },
+  {
+    "revision": "3959d3395e3ad20f847e1c12f7b439b6",
+    "url": "/asialicuos-2020/static/media/whitecloud.3959d339.png"
+  }
+]);
